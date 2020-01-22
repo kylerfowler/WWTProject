434ba9b13e662f7ef2eb5900e35f8d3923e28962
Kyle Fowler <02nails@gmail.com>
2020-01-22 13:00:53 -0600

    Added Course struct

---
+++ WWTProject/Course.swift
@@ -0,0 +1,22 @@
+//
+//  Course.swift
+//  WWTProject
+//
+//  Created by Kyle Fowler on 1/22/20.
+//  Copyright © 2020 Rob Fowler. All rights reserved.
+//
+
+import SwiftUI
+
+struct Course: Identifiable {
+    var id = UUID()
+    
+    var name: String
+    var students: [Student]
+}
+
+struct Student: Identifiable {
+    var id = UUID()
+    
+    var name: String
+}
