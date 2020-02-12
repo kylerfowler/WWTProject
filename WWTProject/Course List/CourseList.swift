f1f096442b1e7ee379af7c392db530fe5069526a
Kyle Fowler <02nails@gmail.com>
2020-02-12 14:22:46 -0600

    Cleaned up folders

---
+++ WWTProject/Course List/CourseList.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  WWTProject
+//
+//  Created by Kyle Fowler on 1/22/20.
+//  Copyright © 2020 Rob Fowler. All rights reserved.
+//
+
+import SwiftUI
+
+struct CourseList: View {
+    @EnvironmentObject var store: CourseStore
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                if store.courses.isEmpty {
+                    VStack {
+                        Spacer()
+                        Text("Classes loading...")
+                        Spacer()
+                        Spacer()
+                    }
+                }
+                
+                ScrollView {
+                    ForEach(store.courses) { course in
+                        ZStack {
+                            NavigationLink(destination: DestinationView(course: course)) {
+                                CourseOverview(course)
+                                    
+                            }
+                            InfoButton(course: course)
+                        }
+                        .padding(.horizontal)
+                        .padding(.vertical, 10)
+                    }
+                }
+            }
+            .edgesIgnoringSafeArea(.bottom)
+            .navigationBarTitle("Classes")
+        }
+        .onAppear {
+            self.store.fetch()
+        }
+    }
+}
+
+struct CourseList_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseList().environmentObject(CourseStore())
+    }
+}
